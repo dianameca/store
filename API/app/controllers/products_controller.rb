94418552e8ece9e8bd53e1b5d4e13fe8def08f03
Diana Meca <dianameca@protonmail.com>
2023-01-02 08:56:00 -0500

    Set up controller for products

--- API/app/controllers/products_controller.rb
+++ API/app/controllers/products_controller.rb
@@ -1,37 +1,37 @@
 class ProductsController < ApplicationController
-  # def index
-  #   @products = Products.all
-  #   render json: @products
-  # end
+  def index
+    @products = Products.all
+    render json: @products
+  end
 
-  # def show
-  #     @product = Product.find(params[:id])
-  #     render json: @product
-  # end
+  def show
+      @product = Product.find(params[:id])
+      render json: @product
+  end
 
-  # def create
-  #     @product = Product.create(
-  #         product_name:params[:product_name],
-  #         product_description:params[:product_description],
-  #         product_keywords: params[:product_keyords]
-  #     )
-  #     render json: @product
-  # end
+  def create
+      @product = Product.create(
+          product_name:params[:product_name],
+          product_description:params[:product_description],
+          product_keywords: params[:product_keywords]
+      )
+      render json: @product
+  end
 
-  # def update
-  #     @product = Product.find(params[:id])
-  #     @product.update(
-  #         product_name:params[:product_name],
-  #         product_description:params[:product_description],
-  #         product_keywords: params[:menu_keywords]
-  #     )
-  #     render json: @product
-  # end
+  def update
+      @product = Product.find(params[:id])
+      @product.update(
+          product_name:params[:product_name],
+          product_description:params[:product_description],
+          product_keywords: params[:product_keywords]
+      )
+      render json: @product
+  end
 
-  # def destroy
-  #     @products = product.all
-  #     @product = product.find(params[:id])
-  #     @secretMenuItem.destroy
-  #     render json: @products
-  # end
+  def destroy
+      @products = product.all
+      @product = product.find(params[:id])
+      @product.destroy
+      render json: @products
+  end
 end
