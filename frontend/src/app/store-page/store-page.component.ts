d85827e91c4220bb30d1c51c47a61480539e1837
Diana Meca <dianameca@protonmail.com>
2023-01-02 09:42:00 -0500

    Add store page component

---
+++ frontend/src/app/store-page/store-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Component({
+  selector: 'app-store-page',
+  templateUrl: './store-page.component.html',
+  styleUrls: ['./store-page.component.css']
+})
+export class StorePageComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+}
